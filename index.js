0698cd7247ff3707ae2c9829aa7f685c66af7186
ernesto <jernestohf@gmail.com>
2026-04-09 17:15:59 -0500

    READY

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+require('dotenv').config();
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -8,64 +9,100 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req,res) => {
+    const gamesUrl = 'https://api.hubapi.com/crm/v3/objects/2-227999689?properties=name,platform,release_date';
+    const headers = {Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,'Content-Type': 'application/json'};
+    try {
+        const resp = await axios.get(gamesUrl, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Games | HubSpot Custom Objects', data });
+    } catch (error) {
+        console.error(error);
+        res.render('homepage', { title: 'Games | HubSpot Custom Objects', data: [] });
+    }
+});
+
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get("/update-cobj", async (req,res) => {
+    let data = null;
+    if (req.query.id) {
+        const gameUrl = `https://api.hubapi.com/crm/v3/objects/2-227999689/${req.query.id}?properties=name,platform,release_date`;
+        const headers = { Authorization: `Bearer ${PRIVATE_APP_ACCESS}`, 'Content-Type': 'application/json' };
+        try {
+            const resp = await axios.get(gameUrl, { headers });
+            data = resp.data.properties;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    res.render("updates", { title: "Update Custom Object Form | Integrating With HubSpot I Practicum.", data });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
+app.post('/update-cobj', async (req, res) => {
+    var platform=req.body.platform;
+    if(Array.isArray(platform)){
+        platform=platform.join(';');
+    } else if (!platform) {
+        platform = '';
     }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
+    const data = {
         properties: {
-            "favorite_book": req.body.newVal
+            "name":req.body.name,
+            "platform":platform,
+            "release_date":req.body.release_date
         }
     }
 
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
+    const name = req.query.name;
+    const gameUrl = `https://api.hubapi.com/crm/v3/objects/2-227999689/`;
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     };
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
+    try {
+        if (req.query.id) {
+            await axios.patch(gameUrl+req.query.id, data, { headers } );
+        }else{
+            await axios.post(gameUrl, data, { headers } );
+        }
+        console.log("OK");
+        res.redirect('/');
     } catch(err) {
+        console.log("ERROR-----");
         console.error(err);
     }
-
 });
-*/
 
+// * Extra Route for delete games
+app.get('/delete-cobj', async (req, res) => {
+    const gameUrl = `https://api.hubapi.com/crm/v3/objects/2-227999689/`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        if (req.query.id) {
+            await axios.delete(gameUrl+req.query.id, { headers } );
+        }
+        res.redirect('/');
+    } catch(err) {
+        console.log("ERROR-----");
+        console.error(err);
+    }
+});
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
